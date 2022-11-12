7e48c4195a3bb52f2a29e0bff613046778df7ac7
alabh <alabenhmouda@gmail.com>
2022-11-12 01:22:26 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ex1';
+  fonts = [
+    'Arial',
+    'Monaco',
+    'Helvetica',
+    'Verdana',
+    'Times New Roman',
+    'Andalé Mono',
+    'Courier',
+    'Bradley Hand',
+    'Brush Script MT',
+    'Luminari',
+  ];
+  color: string = 'white';
+  fontSize: number = 30;
+  fontFamily: string = 'Arial';
 }
